88455a1e04b46678cc900cfee5564032987ff80b
Aku Kangas <kangasaku@gmail.com>
2020-08-27 17:03:08 +0300

    store sample

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState, useCallback, useEffect } from "react";
+import statusFileParser from "utils/statusFileParser";
+
+import { useStoreActions, useStoreState } from "store/state";
 
 function App() {
-  return <div className="App"></div>;
+  const [files, setFiles] = useState();
+
+  const state = useStoreState();
+  const { setPackages } = useStoreActions();
+
+  useEffect(() => {
+    async function parse() {
+      const data = await statusFileParser(files?.[0]);
+      setPackages(data);
+    }
+
+    if (files) {
+      parse();
+    }
+  }, [files, setPackages]);
+
+  const handleChange = useCallback(
+    (e) => {
+      setFiles(e.target.files);
+      console.log(e.target.files?.[0]);
+    },
+    [setFiles]
+  );
+
+  return (
+    <div className="App">
+      <input type="file" onChange={handleChange} files={files} />
+      {/* <ul>
+        {state.packageList.map((e) => (
+          <li>{e}</li>
+        ))}
+      </ul> */}
+      <pre>{JSON.stringify(state, null, 2)}</pre>
+    </div>
+  );
 }
 
 export default App;
