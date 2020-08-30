5af297229964a6f551f76349b2e4b48db4b4ff1a
Aku Kangas <kangasaku@gmail.com>
2020-08-30 22:05:03 +0300

    sub header list item

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -2,10 +2,16 @@ import React, { useMemo } from "react";
 import classes from "utils/classes";
 import styles from "./ListItem.module.scss";
 
-const ListItem = React.memo(({ active, className, ...rest }) => {
+const ListItem = React.memo(({ subHeader, active, className, ...rest }) => {
   const classNames = useMemo(
-    () => classes(className, styles.root, { [styles.active]: active }),
-    [className, active]
+    () =>
+      classes(
+        className,
+        styles.root,
+        { [styles.active]: active },
+        { [styles.subHeader]: subHeader }
+      ),
+    [className, active, subHeader]
   );
 
   return <li className={classNames} {...rest} />;
