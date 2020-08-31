9776a89734676fdd1ebb03bae76a3aab83299164
Aku Kangas <kangasaku@gmail.com>
2020-08-31 10:50:27 +0300

    error handling for unknow files

--- src/components/DragDropFileInput/DragDropFileInput.js
+++ src/components/DragDropFileInput/DragDropFileInput.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { useStoreActions } from "../../store";
 
@@ -10,14 +10,25 @@ import styles from "./DragDropFileInput.module.scss";
 
 export default function DragDropFileInput({ className }) {
   const { setPackages } = useStoreActions();
+  const [error, setError] = useState("");
 
   const [zoneProps, inputProps, { isDragActive }] = useDropzone({
-    onDrop: (files) => statusFileParser(files[0]).then(setPackages),
+    onDrop: (files) =>
+      statusFileParser(files[0])
+        .then(setPackages)
+        .catch((error) =>
+          setError(
+            error.name === "BadFileError"
+              ? "Check input file! Cannot find any package data!"
+              : "Something went wrong."
+          )
+        ),
     zoneProps: { className: classes(styles.root, className) },
   });
 
   return (
     <Dropzone zoneProps={zoneProps} inputProps={inputProps}>
+      {error && <div className={styles.error}>{error}</div>}
       {isDragActive ? "DROP IT!" : "Drag and Drop or select the file"}
     </Dropzone>
   );
