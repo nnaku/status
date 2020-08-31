0252af7eff638aaad8a5b7211bd5e1809234720e
Aku Kangas <kangasaku@gmail.com>
2020-08-31 11:13:15 +0300

    App header styles

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -2,13 +2,20 @@ import React from "react";
 
 import styles from "./AppHeader.module.scss";
 import { useStoreState, useStoreActions } from "../../store";
+import Link from "components/Link";
 function AppHeader() {
   const { hasPackages } = useStoreState();
   const { resetState } = useStoreActions();
 
   return (
     <header className={styles.root}>
-      <h3>status</h3>
+      <div className={styles.title}>
+        <span>/var/lib/dpkg/</span>
+        <span className={styles.status}>
+          <Link href="#">status</Link>
+        </span>
+        <span>explorer</span>
+      </div>
       <div className={styles.spacer} />
       {hasPackages && <button onClick={resetState}>remove file</button>}
     </header>
