8c8150d6a6324ce597482aefc34591cb3746e533
Aku Kangas <kangasaku@gmail.com>
2020-08-31 14:59:21 +0300

    initial state via pros for testing

--- src/store/contex.js
+++ src/store/contex.js
@@ -1,11 +1,15 @@
 import React, { createContext, useReducer } from "react";
 
-import { reducer, initialState, init } from "./state";
+import { reducer, initialState as initState, init } from "./state";
 
 export const StoreContext = createContext({});
 
-export default function StoreProvider(props) {
-  const [state, dispatch] = useReducer(reducer, initialState, init);
+export default function StoreProvider({ initialState, ...rest }) {
+  const [state, dispatch] = useReducer(
+    reducer,
+    { ...initState, ...initialState },
+    init
+  );
 
-  return <StoreContext.Provider value={{ state, dispatch }} {...props} />;
+  return <StoreContext.Provider value={{ state, dispatch }} {...rest} />;
 }
