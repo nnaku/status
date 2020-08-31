b8067dc5650a65eaf17607506abd8a82ffc343b5
Aku Kangas <kangasaku@gmail.com>
2020-08-31 15:40:19 +0300

    Updates

---
+++ precache-manifest.3544ad6912b0dcb05104da291ef6ed06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a99942ae10f244f49c35af81a2dac4d8",
+    "url": "/status/index.html"
+  },
+  {
+    "revision": "e39cc6c0f14d9468b6b8",
+    "url": "/status/static/css/main.1dc1e5db.chunk.css"
+  },
+  {
+    "revision": "095afa133b199f33a174",
+    "url": "/status/static/js/2.fdf3a096.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/status/static/js/2.fdf3a096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e39cc6c0f14d9468b6b8",
+    "url": "/status/static/js/main.5dcaa142.chunk.js"
+  },
+  {
+    "revision": "796bc2019191f6720c4c",
+    "url": "/status/static/js/runtime-main.427de3b6.js"
+  }
+]);
