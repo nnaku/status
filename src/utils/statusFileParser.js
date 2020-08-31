6ae50db5504a48073bfe4aef1cd340484e232261
Aku Kangas <kangasaku@gmail.com>
2020-08-31 10:51:18 +0300

    regexp bug fix

--- src/utils/statusFileParser.js
+++ src/utils/statusFileParser.js
@@ -1,25 +1,29 @@
 const splitPackages = new RegExp(/\n\n/);
 const splitPackageRows = new RegExp(/\n(?!\s)/);
 const rowToKeyValue = new RegExp(/:/); // TODO: Match only first "Key: "
+const pkgVersionData = new RegExp(/\n\s.\n/gm);
 
+// todo: refactor nested array to object e.q {name, isOptional, dataExists??}
 function parseDependsFiel(depends) {
   return depends
     .trim()
-    .replace(/ \(.+\)/gm, "") // get ridoff version data
     .split(", ") //  split each dep to own array element
-    .map((d) => d.split(" | ")); // split alternatives
+    .map((d) =>
+      d
+        .trim() // for "just in case", makesure regexp replace below works.
+        .replace(/ \(.+\)$/, "") // get ridoff version data
+        .split(" | ")
+    ); // split alternatives
 }
 
 function rowKeyValueReducer(acc, curRow) {
   const [key, ...rest] = curRow.split(rowToKeyValue);
-
   const value = rest.join(":").trim();
-
   switch (key) {
     case "Package":
       return { ...acc, name: value };
     case "Description":
-      return { ...acc, description: value };
+      return { ...acc, description: value.replace(pkgVersionData, "\n\n") }; // replace empty line "notation" with real empty line
     case "Depends":
       return { ...acc, depends: value };
     default:
@@ -48,8 +52,18 @@ export default async function statusFileParser(file) {
       ? parseDependsFiel(pkgAsObj.depends)
       : [];
 
-    return { ...acc, [pkgAsObj.name]: { ...pkgAsObj, dependents: null } };
+    // return only "valid" packages
+    if (pkgAsObj.name && pkgAsObj.description && pkgAsObj.depends) {
+      return { ...acc, [pkgAsObj.name]: { ...pkgAsObj, dependents: null } };
+    }
+    return acc;
   }, {});
+
+  if (!Object.keys(pkgObj).length) {
+    let err = new Error("Bad file?");
+    err.name = "BadFileError";
+    throw err;
+  }
 
   return pkgObj;
 }
